0f6f32fc3c663b8b74e324bcc1d0ecbe69b906ff
Marco Heiling <marco.heiling@edu.campus02.at>
2020-07-05 19:16:48 +0200

    added javadoc (@param and @return)

--- src/main/java/at/fhj/iit/IQueue.java
+++ src/main/java/at/fhj/iit/IQueue.java
@@ -9,8 +9,8 @@ public interface IQueue<T> {
    * Adds the element obj to the queue.
    * If the addition is successful, the method returns true else false.
    *
-   * @param obj
-   * @return
+   * @param obj Element to add to the queue
+   * @return true if the element was added
    */
   public abstract boolean offer(T obj);
 
@@ -18,7 +18,7 @@ public interface IQueue<T> {
    * Returns the head (first) element and also deletes it. That is, we cannot get it again.
    * If no element exists (when queue is empty), the method returns null.
    *
-   * @return
+   * @return first element from queue
    */
   public abstract T poll();
 
@@ -26,7 +26,7 @@ public interface IQueue<T> {
    * It also returns and deletes the head element like poll(), but with a small difference.
    * This method throws NoSuchElementException if the queue is empty.
    *
-   * @return
+   * @return first element from queue
    */
   public abstract T remove();
 
@@ -34,7 +34,7 @@ public interface IQueue<T> {
    * Returns the head element but it does not delete it. That is, we can get it again.
    * Returns null when the queue is empty.
    *
-   * @return
+   * @return first element from queue
    */
   public abstract T peek();
 
@@ -42,7 +42,7 @@ public interface IQueue<T> {
    * It works similar to peek() but with a small difference (returns but does not delete the element).
    * It throws NoSuchElementException when the queue is empty.
    *
-   * @return
+   * @return first element from queue
    */
   public abstract T element();
 }
